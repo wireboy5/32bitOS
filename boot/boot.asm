157fc3e43da1efedc0a350b03cad368a4ebd9ba2
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2020-12-08 15:19:06 -0500

    working on getting kernel to work

---
+++ boot/boot.asm
@@ -0,0 +1,44 @@
+mbootmagic equ 0x1BADB002
+mbootflags equ 1 | 2
+mbootcheck equ -(mbootmagic + mbootflags)
+
+; Extern Addresses of sectionsn
+[extern code]
+[extern bss]
+[extern end]
+
+
+[extern multiboot]
+
+multiboot:
+    dd mbootmagic   ; Multiboot Magic
+    dd mbootflags   ; Multiboot Flags
+    dd mbootcheck   ; Multiboot Checksum
+
+    dd multiboot    ; This section
+
+    ; Code, bss, and end sections
+    dd code
+    dd bss
+    dd end
+
+    ; The start of our kernel
+    dd start
+
+
+
+
+; Make sure we can access start
+[global start]
+
+; And that we can access the kernel's entry
+[extern kernel_entry]
+
+start:
+    ; Disable interrupts
+    cli
+    
+    ; Call the kernel
+    call kernel_entry
+    
+    jmp $
--- kernel/C/kernel.c
+++
@@ -1,8 +0,0 @@
-#include "kernel.h"
-
-int kernel_entry(){
-    char* vidMem = (char*)0xB8000;
-    vidMem[0] = 'X';
-
-    return 0;
-}
