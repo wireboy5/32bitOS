9253436bff2a6f26ecffc15c2ab4c5795076c1ac
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2020-12-10 17:55:39 -0500

    Importing some code from another project. Added image.iso to gitignore

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,37 @@
 #include "kernel.h"
 
+/*
+    Screen Colors (For Reference)
+        0 : black
+        1 : blue
+        2 : green
+        3 : cyan
+        4 : red
+        5 : magenta
+        6 : brown
+        7 : light grey
+        8 : dark grey
+        9 : light blue
+        A : light green
+        B : light cyan
+        C : light red
+        D : light magneta
+        E : light brown
+        F : white. 
+*/
+
 int kernel_main(){
-    char* vidmem = (char*)0xB8000;
-    *vidmem = 'x';
     
-    return 0;
+    
+    // Print that we have initialized the heap
+    printf("[%C OK %C] Running in 32 bit mode.",0x02,0x0f);
+
+    // Initializing GDT
+    
+    
+
+	return 0;
+
 }
+
+
--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,3 +1,4 @@
 #pragma once
 
+#include <kernel/x86/drivers/screen.h>
 
