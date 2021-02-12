ec3a7c24810b4384283d71fd83d922108e626d3a
Riley Wilton <meetingprince34@outlook.com>
2021-02-12 18:06:55 -0500

    uhhhhhhh, i forget

---
+++ teachingProgramming/main.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+int main(){
+    // Printing a string
+    printf("Hello, World!\n");
+
+    // Variable
+    int32_t a = 0x20;
+
+    // Another variable
+    int32_t b = 0x111111;
+    
+
+    return 0;
+}
